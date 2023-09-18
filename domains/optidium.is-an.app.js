014d33994c24a0e273490e3db9562d32ab7e1fe4
NotNightSky <100190700+XMODERLIVE@users.noreply.github.com>
2023-09-18 18:48:53 +0530

    Add files via upload

--- domains/optidium.is-an.app.js
+++ domains/optidium.is-an.app.js
@@ -1,12 +1,11 @@
 addSubDomain({
-    description: 'Minecraft Coummunity Project',
-    domain: 'is-an.app',
-    subdomain: 'optidium',
-    owner: {
-      email: 'okmon110@gmail.com',
-    },
-    record: {
-      CNAME: 'optidium.dev',
-    },
-    proxy: true,
-  })
+  description: 'Optidium', // describe your project in this field
+  domain: 'is-an.app', // aka "root-domain". select between '1bt.uk' and 'is-an.app'
+  subdomain: 'optidium', // desired subdomain name
+  owner: {
+    repo: 'https://github.com/XMODERLIVE/Optidium.github.io',
+    email: 'ka237133@gmail.com',
+  },
+  record: {
+    CNAME: 'xmoderlive.github.io',
+  },
